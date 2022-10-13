993714cf0e147e0ed15a0df5b6158a4f6c3b9987
Damibrave <dvictoria614@gmail.com>
2022-10-13 06:29:57 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include <studio.h>
+
+#include <stdio.h>
 
 /**
- * *main - Printing
- * 
- * Return: ALways 0 (successful)
+ * main - Printing
+ *
+ * Return: Always 0 (successful)
  */
 int main(void)
 {
-	puts("\ "Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0)
 }
